7b83f9e341c9204c5ca5816bbf0b2a499927b018
VladyslavP5 <vladyslav.p@blaize.tech>
2024-10-29 18:53:24 +0200

    test: update tests for foundry

--- test/foundry/PositiveEvenSetter.t.sol
+++ test/foundry/PositiveEvenSetter.t.sol
@@ -1,25 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-import "../../lib/forge-std/src/Test.sol";
-import "../../contracts/PositiveEvenSetter.sol";
+import {Test} from "forge-std/Test.sol";
+import {PositiveEvenSetter} from "contracts/PositiveEvenSetter.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PositiveEvenSetterTest is Test {
     PositiveEvenSetter positiveEvenSetter;
 
     address public owner;
+    address public alice;
 
     function setUp() public {
-        owner = address(1);
+        owner = makeAddr("owner");
+        alice = makeAddr("alice");
 
-        vm.startPrank(owner);
+        vm.prank(owner);
 
         positiveEvenSetter = new PositiveEvenSetter();
-
-        vm.stopPrank();
     }
 
-    function testDeployment() public {
+    function testConstructor() public {
         assertEq(address(positiveEvenSetter.owner()), owner);
         assertEq(positiveEvenSetter.positiveEven(), 2);
     }
@@ -27,30 +28,38 @@ contract PositiveEvenSetterTest is Test {
     function testSetNewPositiveEven() public {
         uint256 newPositiveEven = 4;
 
-        vm.startPrank(owner);
+        vm.expectEmit();
+        emit PositiveEvenSetter.PositiveEvenSet(2, newPositiveEven);
+
+        vm.prank(owner);
 
         positiveEvenSetter.setPositiveEven(newPositiveEven);
-
-        vm.stopPrank();
 
         assertEq(positiveEvenSetter.positiveEven(), newPositiveEven);
     }
 
-    function testFail_SetPositiveEvenToZero() public {
-        vm.startPrank(owner);
+    function testSetPositiveEvenToZero() public {
+        vm.expectRevert(PositiveEvenSetter.SetPositiveNumberToZero.selector);
+
+        vm.prank(owner);
 
         positiveEvenSetter.setPositiveEven(0);
     }
 
-    // Test setting an odd number (should revert)
     function testSetPositiveEvenToOdd() public {
-        vm.expectRevert();
+        vm.expectRevert(abi.encodeWithSelector(PositiveEvenSetter.SetEvenToOddNumber.selector, 3));
+
+        vm.prank(owner);
 
         positiveEvenSetter.setPositiveEven(3);
     }
 
-    function testFail_RevertNonOwnerSet() public {
+    function testRevertNonOwnerSet() public {
         uint256 newPositiveEven = 4;
+
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, alice));
+
+        vm.prank(alice);
 
         positiveEvenSetter.setPositiveEven(newPositiveEven);
     }
